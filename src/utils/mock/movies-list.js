553ca6203ad4f04ffa45b592c43733334a697c9f
Behzod Hamdamov <bekzodh332@gmail.com>
2026-03-26 21:34:10 +0500

    feat: install nanoid creat movies-list.js import images in images folder

---
+++ src/utils/mock/movies-list.js
@@ -0,0 +1,303 @@
+import {nanoid} from "nanoid"
+
+import movieImage1 from "../../assets/images/movie-image-1.png"
+import movieImage2 from "../../assets/images/movie-image-2.png"
+import movieImage3 from "../../assets/images/movie-image-3.png"
+import movieImage4 from "../../assets/images/movie-image-4.png"
+import movieImage5 from "../../assets/images/movie-image-5.png"
+import movieImage6 from "../../assets/images/movie-image-6.png"
+import movieImage7 from "../../assets/images/movie-image-7.png"
+import movieImage8 from "../../assets/images/movie-image-8.png"
+import movieImage9 from "../../assets/images/movie-image-9.png"
+import movieImage10 from "../../assets/images/movie-image-10.png"
+import movieImage11 from "../../assets/images/movie-image-11.png"
+import movieImage12 from "../../assets/images/movie-image-12.png"
+import movieImage13 from "../../assets/images/movie-image-13.png"
+import movieImage14 from "../../assets/images/movie-image-14.png"
+import movieImage15 from "../../assets/images/movie-image-15.png"
+import movieImage16 from "../../assets/images/movie-image-16.png"
+import movieImage17 from "../../assets/images/movie-image-17.png"
+import movieImage18 from "../../assets/images/movie-image-18.png"
+import movieImage19 from "../../assets/images/movie-image-19.png"
+import movieImage20 from "../../assets/images/movie-image-20.png"
+import movieImage21 from "../../assets/images/movie-image-21.png"
+import movieImage22 from "../../assets/images/movie-image-22.png"
+import movieImage23 from "../../assets/images/movie-image-23.png"
+import movieImage24 from "../../assets/images/movie-image-24.png"
+
+import trendingMovieImage1 from "../../assets/images/trending-movie-image-1.png"
+import trendingMovieImage2 from "../../assets/images/trending-movie-image-2.png"
+import trendingMovieImage3 from "../../assets/images/trending-movie-image-3.png"
+
+export let moviesList = [
+  {
+    id: nanoid(),
+    movieName: "The Great Lands",
+    movieYear: 2019,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage1,
+  },
+  {
+    id: nanoid(),
+    movieName: "The Diary",
+    movieYear: 2019,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage2,
+  },
+  {
+    id: nanoid(),
+    movieName: "Earth’s Untouched",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "18+",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage3,
+  },
+  {
+    id: nanoid(),
+    movieName: "No Land Beyond",
+    movieYear: 2019,
+    movieType: "Movie",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage4,
+  },
+  {
+    id: nanoid(),
+    movieName: "During The Hunt",
+    movieYear: 2016,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage5,
+  },
+  {
+    id: nanoid(),
+    movieName: "Autosport The Series",
+    movieYear: 2016,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage6,
+  },
+  {
+    id: nanoid(),
+    movieName: "Same Answer II",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage7,
+  },
+  {
+    id: nanoid(),
+    movieName: "Below Echo",
+    movieYear: 2016,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage8,
+  },
+  {
+    id: nanoid(),
+    movieName: "The Rockies",
+    movieYear: 2015,
+    movieType: "TV Series",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage9,
+  },
+  {
+    id: nanoid(),
+    movieName: "Relentless",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage10,
+  },
+  {
+    id: nanoid(),
+    movieName: "Community of Ours",
+    movieYear: 2018,
+    movieType: "TV Series",
+    movieRestriction: "18+",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage11,
+  },
+  {
+    id: nanoid(),
+    movieName: "Van Life",
+    movieYear: 2015,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage12,
+  },
+  {
+    id: nanoid(),
+    movieName: "The Heiress",
+    movieYear: 2021,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage13,
+  },
+  {
+    id: nanoid(),
+    movieName: "Off the Track",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "18+",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage14,
+  },
+  {
+    id: nanoid(),
+    movieName: "Whispering Hill",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage15,
+  },
+  {
+    id: nanoid(),
+    movieName: "112",
+    movieYear: 2013,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage16,
+  },
+  {
+    id: nanoid(),
+    movieName: "Lone Heart",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage17,
+  },
+  {
+    id: nanoid(),
+    movieName: "Production Line",
+    movieYear: 2018,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage18,
+  },
+  {
+    id: nanoid(),
+    movieName: "Dogs",
+    movieYear: 2016,
+    movieType: "TV Series",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage19,
+  },
+  {
+    id: nanoid(),
+    movieName: "Asia in 24 Days",
+    movieYear: 2020,
+    movieType: "TV Series",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage20,
+  },
+  {
+    id: nanoid(),
+    movieName: "The Tasty Tour",
+    movieYear: 2016,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage21,
+  },
+  {
+    id: nanoid(),
+    movieName: "Darker",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "18+",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage22,
+  },
+  {
+    id: nanoid(),
+    movieName: "Unresolved Cases",
+    movieYear: 2018,
+    movieType: "TV Series",
+    movieRestriction: "18+",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage23,
+  },
+  {
+    id: nanoid(),
+    movieName: "Mission: Saturn",
+    movieYear: 2017,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: false,
+    movieImg: movieImage24,
+  },
+  {
+    id: nanoid(),
+    movieName: "Beyond Earth",
+    movieYear: 2019,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: true,
+    movieImg: trendingMovieImage1,
+  },
+  {
+    id: nanoid(),
+    movieName: "Bottom Gear",
+    movieYear: 2021,
+    movieType: "Movie",
+    movieRestriction: "PG",
+    isBooked: false,
+    isTrending: true,
+    movieImg: trendingMovieImage2,
+  },
+  {
+    id: nanoid(),
+    movieName: "Undiscovered Cities",
+    movieYear: 2019,
+    movieType: "TV Series",
+    movieRestriction: "E",
+    isBooked: false,
+    isTrending: true,
+    movieImg: trendingMovieImage3,
+  },
+]
